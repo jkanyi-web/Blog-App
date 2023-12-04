9c9fd68b7230090e5fbcad3497dcedd16dabc246
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-12-04 13:42:55 +0300

    review changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  get '/users/:user_id/posts', to: 'posts#index'
-  get '/users/:user_id/posts/:id', to: 'posts#show'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
