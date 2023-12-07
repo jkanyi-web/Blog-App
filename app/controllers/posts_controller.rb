a832d93ac389dfdb5bd75844c0228fc85599cfb8
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-12-07 10:34:55 +0300

    rails best practices implemented

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,13 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.page(params[:page]).per(2)
+    @posts_count = @posts.count
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
+    @comments_count = @post.comments.count
+    @likes_count = @post.likes.count
   end
 end
