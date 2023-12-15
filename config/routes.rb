e9d120a510e3b4deffdc845c63305f81c67e02aa
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-12-15 17:42:06 +0300

    Create an API endpoint to list all posts for a user

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,14 @@ Rails.application.routes.draw do
     end
   end
 
+  # API routes
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :posts, only: [:index]
+      end
+    end
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
