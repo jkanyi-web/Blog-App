00edcdbc0a340397ef8a8422ecea5a9a79e635e7
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-12-16 16:51:17 +0300

    Create an API endpoint to list all comments for a user's post

--- config/routes.rb
+++ config/routes.rb
@@ -22,10 +22,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users do
-        resources :posts, only: [:index]
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index]
+        end
       end
     end
   end
-
-  get "up" => "rails/health#show", as: :rails_health_check
 end
